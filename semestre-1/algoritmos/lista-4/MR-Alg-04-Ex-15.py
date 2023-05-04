39001646df4d29438165996059b53843d8db6ab5
mickaelrei <mickael.reichert@gmail.com>
2023-05-04 17:55:00 -0300

    Atualizando

--- semestre-1/algoritmos/lista-4/MR-Alg-04-Ex-15.py
+++ semestre-1/algoritmos/lista-4/MR-Alg-04-Ex-15.py
@@ -1,11 +1,3 @@
-'''Inicialize result como uma string vazia
-Seja q o número decimal a ser convertido
-Repita
-r recebe o resto da divisão de q por 2
-converta r para uma string e adicione no início de result
-faça a divisão inteira de q por 2 (descartando o resto) e guarde o resultado em q
-Até que q seja igual a zero'''
-
 decimal = int(input("Digite um número decimal: "))
 
 binario = ""
