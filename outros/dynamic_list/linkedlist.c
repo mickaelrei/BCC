31515bbe804bdf2fb85b39edfff4c7866024d45b
mickaelrei <mickael.reichert@gmail.com>
2023-05-23 12:45:29 -0300

    Finalizando

--- outros/dynamic_list/linkedlist.c
+++ outros/dynamic_list/linkedlist.c
@@ -1,210 +1,243 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 typedef struct reg {
-    int conteudo;
-    struct reg *prox;
-} celula;
+    int value;
+    struct reg *next;
+} cell;
 
-celula *le_create() {
-    celula *le = malloc(sizeof(celula));
-    le->prox = NULL;
+cell *ll_create() {
+    cell *ll = malloc(sizeof(cell));
+    ll->next = NULL;
 
-    return le;
+    return ll;
 }
 
-int le_len(celula *le) {
-    if (le == NULL)
+int ll_len(cell *ll) {
+    if (ll == NULL)
         return -1;
     
     int i = 0;
-    celula *p = le->prox;
+    cell *p = ll->next;
     while (p != NULL) {
-        p = p->prox;
+        p = p->next;
         i++;
     }
 
     return i;
 }
 
-int le_index(celula *le, int value, int start) {
-    if (le == NULL || start < 0 || start >= le_len(le))
+int ll_index(cell *ll, int value, int start) {
+    if (ll == NULL || start < 0 || start >= ll_len(ll))
         return -1;
 
-    int i = 0, achou = 0;
-    celula *p = le->prox;
+    int i = 0, found = 0;
+    cell *p = ll->next;
     while (p != NULL) {
-        if (i >= start && p->conteudo == value) {
-            achou = 1;
+        if (i >= start && p->value == value) {
+            found = 1;
             break;
         }
-        p = p->prox;
+        p = p->next;
         i++;
     }
 
-    if (achou) return i;
+    if (found) return i;
     else return -1;
 }
 
-celula *le_get_at(celula *le, int index) {
-    if (index < 0 || le == NULL)
+cell *ll_get_at(cell *ll, int index) {
+    if (index < 0 || ll == NULL)
         return NULL;
         
     int i = 0;
-    celula *p = le->prox;
+    cell *p = ll->next;
 
     while (p != NULL && i != index) {
-        p = p->prox;
+        p = p->next;
         i++;
     }
 
     return p;
 }
 
-void le_print(celula *le) {
-    if (le == NULL)
+void ll_set_at(cell *ll, int index, int value) {
+    ll_get_at(ll, index)->value = value;
+}
+
+void ll_print(cell *ll) {
+    if (ll == NULL)
         return;
 
-    celula *p;
-    for (p = le->prox; p != NULL; p = p->prox) {
-        printf("%d", p->conteudo);
-        if (p->prox != NULL)
+    printf("[");
+    cell *p;
+    for (p = ll->next; p != NULL; p = p->next) {
+        printf("%d", p->value);
+        if (p->next != NULL)
             printf(", ");
     }
     
-    printf("\n");
+    printf("]\n");
 }
 
-void le_append(celula *le, int value) {
-    if (le == NULL)
+void ll_append(cell *ll, int value) {
+    if (ll == NULL)
         return;
 
-    celula *p;
-    p = malloc(sizeof(celula));
-    p->conteudo = value;
-    p->prox = NULL;
+    cell *p;
+    p = malloc(sizeof(cell));
+    p->value = value;
+    p->next = NULL;
 
-    celula *p1 = le;
-    while (p1->prox != NULL)
-        p1 = p1->prox;
+    cell *p1 = ll;
+    while (p1->next != NULL)
+        p1 = p1->next;
 
 
-    p1->prox = p;
+    p1->next = p;
 }
 
-void le_insert(celula *le, int pos, int value) {
-    if (le == NULL || pos < 0 || pos > le_len(le))
+void ll_insert(cell *ll, int pos, int value) {
+    if (ll == NULL || pos < 0 || pos >= ll_len(ll))
         return;
 
-    if (pos == le_len(le)) {
-        le_append(le, value);
+    if (pos == ll_len(ll)) {
+        ll_append(ll, value);
         return;
     }
 
-    celula *p = malloc(sizeof(celula));
-    p->conteudo = value;
+    cell *p = malloc(sizeof(cell));
+    p->value = value;
 
-    // Atual e anterior
-    celula *curr = le_get_at(le, pos);
-    celula *prev;
+    // Current and previous
+    cell *curr = ll_get_at(ll, pos);
+    cell *prev;
     if (pos == 0)
-        prev = le;
+        prev = ll;
     else
-        prev = le_get_at(le, pos - 1);
+        prev = ll_get_at(ll, pos - 1);
 
-    prev->prox = p;
-    p->prox = curr;
+    prev->next = p;
+    p->next = curr;
 }
 
-celula *le_find(celula *le, int value) {
-    if (le == NULL)
+cell *ll_find(cell *ll, int value, int start) {
+    if (ll == NULL || start < 0 || start >= ll_len(ll))
         return NULL;
 
-    celula *p = le->prox;
-    while (p != NULL && p->conteudo != value)
-        p = p->prox;
+    cell *p = ll->next;
+    int i = 0;
+    while (p != NULL && (p->value != value || i < start)) {
+        p = p->next;
+        i++;
+    }
     
     return p;
 }
 
-void le_remove(celula *le, int value) {
-    if (le == NULL)
+void ll_remove(cell *ll, int pos) {
+    if (ll == NULL || pos < 0 || pos >= ll_len(ll))
         return;
-        
-    celula *p, *q;
-    p = le;
-    q = le->prox;
-    while (q != NULL && q->conteudo != value) {
+
+    cell *p, *q;
+    p = ll;
+    q = ll->next;
+    int i = 0;
+    while (q != NULL && i != pos) {
         p = q;
-        q = q->prox;
+        q = q->next;
+        i++;
     }
+
     if (q != NULL) {
-        p->prox = q->prox;
+        p->next = q->next;
         free(q);
     }
 }
 
-void le_swap(celula *le, int pos0, int pos1) {
-    if (pos0 == pos1 || le == NULL)
+cell *ll_slice(cell *ll, int start, int size) {
+    int len = ll_len(ll);
+    if (ll == NULL || start < 0 || start >= len)
+        return NULL;
+
+    cell *new_ll = ll_create();
+
+    size = fmin(size, len - start);
+
+    if (size == 0)
+        return new_ll;
+
+    cell *st = ll_get_at(ll, start);
+    for (int i = 0; i < size; i++) {
+        ll_append(new_ll, st->value);
+        st = st->next;
+    }
+
+    return new_ll;
+}
+
+void ll_swap(cell *ll, int pos0, int pos1) {
+    if (pos0 == pos1 || ll == NULL)
         return;
     
-    celula *p0 = le_get_at(le, pos0);
-    celula *p1 = le_get_at(le, pos1);
+    cell *p0 = ll_get_at(ll, pos0);
+    cell *p1 = ll_get_at(ll, pos1);
 
     if (p0 == NULL || p1 == NULL)
         return;
 
-    celula *p0_prev = le_get_at(le, pos0 - 1);
-    celula *p1_prev = le_get_at(le, pos1 - 1);
-    celula *p0_prox = p0->prox;
-    celula *p1_prox = p1->prox;
-    int colados = abs(pos0 - pos1) == 1;
+    cell *p0_prev = ll_get_at(ll, pos0 - 1);
+    cell *p1_prev = ll_get_at(ll, pos1 - 1);
+    cell *p0_next = p0->next;
+    cell *p1_next = p1->next;
+    int close = abs(pos0 - pos1) == 1;
 
     // Se pos0 ou pos1 forem zero, o anterior do mesmo
     // deve ser a cabeça da lista
     if (pos0 == 0)
-        p0_prev = le;
+        p0_prev = ll;
     else if (pos1 == 0)
-        p1_prev = le;
+        p1_prev = ll;
 
     // A ordem de mudança de endereços é diferente se pos0 < pos1 ou pos0 > pos1
     if (pos0 < pos1) {
-        p0_prev->prox = p1;
-        p1->prox = colados ? p0 : p0->prox;
-        p1_prev->prox = p0;
-        p0->prox = p1_prox;
+        p0_prev->next = p1;
+        p1->next = close ? p0 : p0->next;
+        p1_prev->next = p0;
+        p0->next = p1_next;
     } else {
-        p1_prev->prox = p0;
-        p0->prox = colados ? p1 : p1->prox;
-        p0_prev->prox = p1;
-        p1->prox = p0_prox;
+        p1_prev->next = p0;
+        p0->next = close ? p1 : p1->next;
+        p0_prev->next = p1;
+        p1->next = p0_next;
     }
 }
 
-void le_clear(celula *le) {
-    if (le == NULL)
+void ll_clear(cell *ll) {
+    if (ll == NULL)
         return;
 
-    celula *p = le->prox;
+    cell *p = ll->next;
     if (p == NULL)
         return;
 
-    le->conteudo = 0;
-    le->prox = NULL;
-    free(le->prox);
-    le_clear(p);
+    ll->value = 0;
+    ll->next = NULL;
+    ll_clear(p);
+    free(ll->next);
 }
 
-void le_bubblesort(celula *le) {
-    if (le == NULL)
+void ll_bubblesort(cell *ll) {
+    if (ll == NULL)
         return;
     
-    int size = le_len(le);
+    int size = ll_len(ll);
     for (int i = size; i >= 0; i--) {
         for (int j = 0; j < i - 1; j++) {
-            if (le_get_at(le, j)->conteudo > le_get_at(le, j + 1)->conteudo)
-                le_swap(le, j, j + 1);
+            if (ll_get_at(ll, j)->value > ll_get_at(ll, j + 1)->value)
+                ll_swap(ll, j, j + 1);
         }
     }
 }
@@ -217,30 +250,28 @@ void main() {
     srand(time(NULL));
 
     // Cria lista encadeada vazia
-    celula *le = le_create();
+    cell *ll = ll_create();
 
     // Insere valores aleatórios
     for (int i = 0; i < 10; i++) {
-        le_append(le, random(-15, 15));
-        le_insert(le, 0, i);
+        ll_append(ll, random(-15, 15));
     }
 
     // Mostra a lista original
-    le_print(le);
-    printf("Tamanho: %d\n", le_len(le));
+    ll_print(ll);
+    printf("Tamanho: %d\n\n", ll_len(ll));
 
-    // le_insert(le, 2, 15);
-
-    // Ordena
-    le_bubblesort(le);
-
-    // Mostra a lista ordenada
-    le_print(le);
-    printf("Tamanho: %d\n", le_len(le));
+    // Cria slice
+    cell *slice = ll_slice(ll, 8, 18);
+    ll_print(slice);
+    printf("Tamanho slice: %d\n", ll_len(slice));
 
     // Limpa
-    le_clear(le);
-    printf("Cleared\n");
-    printf("Tamanho: %d\n", le_len(le));
-    free(le);
+    ll_clear(ll);
+    printf("\nCleared\n");
+    printf("Tamanho: %d\n", ll_len(ll));
+    free(ll);
+
+    ll_clear(slice);
+    free(slice);
 }
